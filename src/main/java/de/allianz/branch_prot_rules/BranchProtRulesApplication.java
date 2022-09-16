639df089340d721ff405bdc403838ced539e27f8
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-16 13:14:38 +0200

    Create maven project

---
+++ src/main/java/de/allianz/branch_prot_rules/BranchProtRulesApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.branch_prot_rules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchProtRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchProtRulesApplication.class, args);
+	}
+
+}
